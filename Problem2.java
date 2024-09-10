648b1c3ba946001db17934cc75430e006350bed5
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-10 11:10:39 -0700

    prob2done

--- Problem2.java
+++ Problem2.java
@@ -2,7 +2,13 @@ public class Problem2
 {
   public static void main(String[] args)
   {
-    
+     double x = 57.3934;
+     double y = 22.5211;
+     int num1 = (int)(x+0.5);
+     int num2 = (int)(y+0.5);
+     int sum = num1 + num2;
+     System.out.println("The original numbers are " + x + " and " + y);
+     System.out.println("The rounded sum is " + sum);
   }
 }
 
