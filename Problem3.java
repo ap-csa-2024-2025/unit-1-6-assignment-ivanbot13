851c179343f06eae943e0acd00089c7c3c88094d
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-11 10:43:57 -0700

    prob3

--- Problem3.java
+++ Problem3.java
@@ -3,8 +3,14 @@ public class Problem3
   public static void main(String[] args)
   {
     double num = 67.3424;
-    num %= 1;
-    System.out.println((int)(num*1000));
+    double x = num%1;
+    x *= 1000;
+
+    int firDigit = (int)(x/100);
+    int secDigit = (int)((x/10)%10);
+    int thrDigit = (int)(x%10);
+    System.out.println("decimal number: " + num);
+    System.out.println("Answer: " + firDigit + " " + secDigit + " " + thrDigit);
   }
 }
 
